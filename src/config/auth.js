385d3b8569858c1a21464dc3ad642b4c8af7df99
André Luís Batista Kunde <andrelbkunde@gmail.com>
2019-11-14 16:15:46 -0300

    Users, sessions and seed admin

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '',
+  secret: 'ec571e1438330841aa77e34c8d4ed102',
   expiresIn: '7d',
 };
--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  port: 5433,
+  host: '192.168.99.100',
+  port: 5432,
   username: 'postgres',
-  password: '1234',
+  password: '12345678',
   database: 'gympoint',
   define: {
     timestamps: true,
