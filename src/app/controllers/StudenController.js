b2d4dddc9dca24db0c380c6a9521b9389f39b5ea
André Luís Batista Kunde <andrelbkunde@gmail.com>
2019-11-14 16:26:56 -0300

    Begining of students

---
+++ src/app/controllers/StudenController.js
@@ -0,0 +1,11 @@
+class StudentController {
+  async store(req, res) {
+    return res.json();
+  }
+
+  async update(req, res) {
+    return res.json();
+  }
+}
+
+export default new StudentController();
